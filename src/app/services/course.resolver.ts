815eee115cbd9e84c4c6fbc6fa27aa16003b78fd
Todd Forsberg <tforsberg@gmail.com>
2018-12-19 13:56:32 -0600

    update

--- src/app/services/course.resolver.ts
+++ src/app/services/course.resolver.ts
@@ -1,18 +1,20 @@
 
 
 
-import {Injectable} from "@angular/core";
-import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
-import {Course} from "../model/course";
-import {Observable} from "rxjs/Observable";
-import {CoursesService} from "./courses.service";
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+
+import {Observable} from 'rxjs/Observable';
+
+import {Course} from '../model/course';
+import {CoursesService} from './courses.service';
 
 
 
 @Injectable()
 export class CourseResolver implements Resolve<Course> {
 
-    constructor(private coursesService:CoursesService) {
+    constructor(private coursesService: CoursesService) {
 
     }
 
